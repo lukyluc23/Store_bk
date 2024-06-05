52beacdafb23bf3917e4377aa6ebd31d94070abe
lucas <luky.luc.23@gmail.com>
2024-06-05 19:58:45 -0300

    inizialice bk sotre proyect

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Application } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { corsOptions } from "./config/cors";
+// import apiRoutes from "./routes/api.routes";
+// import baseRoutes from "./routes/base.routes";
+// import { handleNotFound } from "./middlewares/errorHandler";
+
+// -----------------------------------------------------------------------------
+
+dotenv.config();
+
+const app: Application = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// Register Base route
+// app.use("/", baseRoutes);
+
+// // Register API routes
+// app.use("/api", apiRoutes);
+
+// // Handle 404 errors
+// app.use(handleNotFound);
+
+export default app;
